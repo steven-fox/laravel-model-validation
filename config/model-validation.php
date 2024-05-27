8702e4e61c6d86fe1d0816a17f03a341a15377a8
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-05-27 10:01:26 -0400

    run configure

---
+++ config/model-validation.php
@@ -0,0 +1,6 @@
+<?php
+
+// config for StevenFox/LaravelModelValidation
+return [
+
+];
---
+++ src/LaravelModelValidationServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace StevenFox\LaravelModelValidation;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LaravelModelValidationServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('laravel-model-validation')
+            ->hasConfigFile();
+    }
+}
